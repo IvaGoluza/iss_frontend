a7cef58a4ed67fd63518b9ef0661bfd157cdb67e
Iva Goluža <ig53401@fer.hr>
2024-05-22 22:25:44 +0200

    project init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+
+import Router from "./Router";
+
+const queryClient = new QueryClient();
+
+export default function App() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
+        </QueryClientProvider>
+    );
+}
---
+++ src/Router.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Route, Routes } from "react-router-dom";
+
+import ApplicationFrame from "./pages/ApplicationFrame";
+import Home from "./pages/Home/Home";
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<ApplicationFrame />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+      <Route path="*" element={<div className="h1">Page not found</div>} />
+    </Routes>
+  );
+}
